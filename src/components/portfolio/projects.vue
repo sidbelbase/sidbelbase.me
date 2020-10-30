91be43969b9dd78b7c4a2f4dc032a4d090de813d
Siddharth Belbase <siddharthbelbase@gmail.com>
2020-10-30 15:53:03 +0545

    [feat] integrate with github APIs

--- src/components/portfolio/projects.vue
+++ src/components/portfolio/projects.vue
@@ -1,229 +1,104 @@
 <template>
-  <section
-    class="resume-content p-3 p-lg-5 d-flex justify-content-center"
-    id="projects"
-  >
+  <section class="resume-content p-3 p-lg-5 d-flex justify-content-center" id="projects">
     <div class="w-100">
       <h2 class="mb-5 text-darker">Projects</h2>
-
       <div
         class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
+        v-for="(repo, index) in projects"
+        :key="index"
       >
         <div class="resume-content">
-          <h3 class="mb-0">covidnepal</h3>
+          <h3 class="mb-0">{{ repo.name }}</h3>
           <div class="subheading mb-3 text-darker">
-            <span><i class="fab fa-vuejs"></i> Vue Framework </span>
-            <span><i class="fab fa-python ml-2"></i> Python </span>
+            <span>
+              <i :class="getIconFrom(repo.language)"></i>
+              <span class="mx-2">{{ repo.language }}</span>
+            </span>
           </div>
-          <p>
-            Covidnepal is just "another try" to look into covidcases inside
-            Nepal. It was made to cover reliable, detailed and more
-            informational data as possible inside a same application. It depends
-            on reliable API (after spending months for it). This application
-            uses PWA (Progressive Web App) technology, meaning it could be
-            installed inside the browser easily like a mobile application and
-            later could be run like an application. This application also
-            follows neumorphic design style for UI & smoother animations for
-            better UX.
-          </p>
+          <p>{{ repo.description }}</p>
+          <a :href="repo.url" class="links text-decoration-none font-weight-bold">Github</a>
+          <span v-if="repo.homepage" class="mx-1 font-weight-bold">&middot;</span>
           <a
-            href="https://github.com/sidbelbase/covidnepal"
+            :href="repo.homepage"
+            v-if="repo.homepage"
             class="links text-decoration-none font-weight-bold"
-            >Git Repository</a
-          >
-          <span>|</span>
-          <a
-            href="https://t.me/covidnepal_bot"
-            class="links text-decoration-none font-weight-bold"
-            >Telegram Bot</a
-          >
-          <span>|</span>
-          <a
-            href="https://covidnepal.now.sh"
-            class="links text-decoration-none font-weight-bold"
-            >Vercel</a
-          >
+          >Website</a>
+          <span :title="countStars(repo.stargazers_count)">
+            <span class="mx-1 font-weight-bold">&middot;</span>
+            <span>
+              <i class="fa fa-star text-darker"></i>
+            </span>
+            <span class="mx-1 text-darker font-weight-bold">{{repo.stargazers_count}}</span>
+          </span>
         </div>
         <div class="resume-date text-md-right">
-          <span class="text-darker font-weight-bold">July, 2020</span>
-        </div>
-      </div>
-      <div
-        class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
-      >
-        <div class="resume-content">
-          <h3 class="mb-0">Effintodos</h3>
-          <div class="subheading mb-3 text-darker">
-            <span><i class="fab fa-vuejs"></i> Vue Framework</span>
-          </div>
-          <p>
-            Effintodos is a simple todo app that helps you complete your tasks
-            like they are supposed to. It eliminates the typical strikethrough
-            and checkbox styles. So like in the image above it does them in
-            post-it note style. It is inspired from a youtube tutorial but
-            developed further to fit both UI & UX in mind. It is intuitive and
-            simple to use bundled into a SPA (Single-Page Applications) with the
-            use of the Vue framework along with added user authentication.
-          </p>
-          <a
-            href="https://github.com/sidbelbase/effintodos"
-            class="links text-decoration-none font-weight-bold"
-            >Git Repository</a
-          >
-          <span>|</span>
-          <a
-            href="https://effintodos.now.sh"
-            class="links text-decoration-none font-weight-bold"
-            >Vercel</a
-          >
-        </div>
-        <div class="resume-date text-md-right">
-          <span class="text-darker font-weight-bold">June, 2020</span>
-        </div>
-      </div>
-      <div
-        class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
-      >
-        <div class="resume-content">
-          <h3 class="mb-0">Undistracted Me</h3>
-          <div class="subheading mb-3 text-darker">
-            <span><i class="fab fa-js"></i> Javascript </span>
-            <span><i class="fab fa-html5 ml-2"></i> Html </span>
-          </div>
-          <p>
-            A distraction-free nepali date & clock for your browser. This is
-            just a simple extension that replaces your new tab page with
-            real-time, distraction-free & yet minimal nepalidate. It also
-            features a live clock & week days. In the future this app will be
-            able to add extra customization features. It's web demo version is
-            deployed in Vercel.
-          </p>
-          <a
-            href="https://github.com/sidbelbase/undistracted-me"
-            class="links text-decoration-none font-weight-bold"
-            >Git Repository</a
-          >
-          <span>|</span>
-          <a
-            href="https://whatsthemiti.now.sh"
-            class="links text-decoration-none font-weight-bold"
-            >Vercel</a
-          >
-        </div>
-        <div class="resume-date text-md-right">
-          <span class="text-darker font-weight-bold">May, 2020</span>
-        </div>
-      </div>
-      <div
-        class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
-      >
-        <div class="resume-content">
-          <h3 class="mb-0">What'S The Miti?</h3>
-          <div class="subheading mb-3 text-darker">
-            <span><i class="fab fa-html5"></i> Html </span>
-            <span><i class="fab fa-python ml-2"></i> Python </span>
-          </div>
-          <p>
-            A distractionless nepali date & clock for your browser. This is just
-            a simple app that shows nepali-date for you. I was just frustrated
-            typing hamropatro or nepalipatro, hence my app. You're free to use
-            it's api. Go on github to find more, also it is deployed in Vercel.
-          </p>
-          <a
-            href="https://github.com/sidbelbase/whats-the-miti"
-            class="links text-decoration-none font-weight-bold"
-            >Git Repository</a
-          >
-          <span>|</span>
-          <a
-            href="https://whatsthemiti.now.sh"
-            class="links text-decoration-none font-weight-bold"
-            >Vercel</a
-          >
-        </div>
-        <div class="resume-date text-md-right">
-          <span class="text-darker font-weight-bold">May, 2020</span>
-        </div>
-      </div>
-      <div
-        class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
-      >
-        <div class="resume-content">
-          <h3 class="mb-0">Who Called Me?</h3>
-          <div class="subheading mb-3 text-darker">
-            <span><i class="fab fa-html5"></i> Html </span>
-            <span><i class="fab fa-python ml-2"></i> Python </span>
-          </div>
-          <p>
-            A web app based on python's framework called flask that checks
-            whether any number falls under the major carrier in Nepal ranging
-            among Ncell, Namaste, UTL, SmartCell, HelloNepal or Landline. It
-            also locates the number was registered in based on numerical
-            numbering. It is deployed in Vercel.
-          </p>
-          <a
-            href="https://github.com/sidbelbase/who-called-me"
-            class="links text-decoration-none font-weight-bold"
-            >Git Repository</a
-          >
-          <span>|</span>
-          <a
-            href="https://whocalledme.now.sh"
-            class="links text-decoration-none font-weight-bold"
-            >Vercel</a
-          >
-        </div>
-        <div class="resume-date text-md-right">
-          <span class="text-darker font-weight-bold">April, 2020</span>
-        </div>
-      </div>
-      <div
-        class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
-      >
-        <div class="resume-content">
-          <h3 class="mb-0">Nepali Datetime Applet</h3>
-          <div class="subheading mb-3 text-darker">
-            <span><i class="fab fa-python"></i> Python </span>
-          </div>
-          <p>
-            A simple taskbar applet to show realtime datetime along with nepali
-            date for linux desktops. It shows calendar like in nepali calendar
-            system.
-          </p>
-          <a
-            href="https://github.com/sidbelbase/nepali-datetime-applet"
-            class="links text-decoration-none font-weight-bold"
-            >Github Repository</a
-          >
-        </div>
-        <div class="resume-date text-md-right">
-          <span class="text-darker font-weight-bold">March, 2020</span>
-        </div>
-      </div>
-      <div
-        class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
-      >
-        <div class="resume-content">
-          <h3 class="mb-0">Virus Checker</h3>
-          <div class="subheading mb-3 text-darker">
-            <span><i class="fa fa-terminal"></i> Batchscript </span>
-          </div>
-          <p>
-            A simple batchscript for windows operating system that deletes files
-            that looks goofy to you. It won't automatically delete them, but
-            you'll be able to see what's inside and out. This is handy for those
-            people who dont want heavy antiviruses popping here and there.
-          </p>
-          <a
-            href="https://github.com/sidbelbase/VirusChecker"
-            class="links text-decoration-none font-weight-bold"
-            >Github Repository</a
-          >
-        </div>
-        <div class="resume-date text-md-right">
-          <span class="text-darker font-weight-bold">April, 2018</span>
+          <span class="text-darker font-weight-bold">{{ formatDate(repo.created_at) }}</span>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+import relativeTime from "dayjs/plugin/relativeTime";
+import icons from "@/data/icons.json";
+import { ref, onMounted } from "vue";
+import dayjs from "dayjs";
+import axios from "axios";
+export default {
+  setup() {
+    let projects = ref([]);
+    const username = ref("sidbelbase");
+    const url = ref(`https://api.github.com/users/${username.value}/repos`);
+
+    function humanize(givendate) {
+      dayjs.extend(relativeTime);
+      return dayjs(givendate).fromNow();
+    }
+
+    function formatDate(givendate) {
+      return dayjs(givendate).format("MMMM, YYYY");
+    }
+
+    function getIconFrom(language) {
+      for (let lang in icons) {
+        if (icons.hasOwnProperty(lang) && language.toLowerCase() === lang) {
+          return icons[lang];
+        }
+      }
+    }
+
+    function countStars(stars) {
+      return +stars + " stars on Github!";
+    }
+
+    function fetchRepos() {
+      axios
+        .get(url.value)
+        .then((responses) => {
+          responses.data.forEach((repo) => {
+            projects.value.push({
+              name: repo.name,
+              description: repo.description,
+              created_at: repo.created_at,
+              stargazers_count: repo.stargazers_count,
+              language: repo.language,
+              homepage: repo.homepage,
+              url: repo.html_url,
+              fork: repo.fork,
+            });
+          });
+          projects.value = projects.value
+            .filter((repo) => !repo.fork && repo.language)
+            .sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+          console.log(projects);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    onMounted(() => fetchRepos());
+    return { projects, url, humanize, formatDate, getIconFrom, countStars };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import { createApp } from 'vue'
 import App from './app.vue'
 import router from './router'
-import "bootstrap/dist/css/bootstrap.css";
+import { createApp } from 'vue'
 import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-createApp(App).use(router).mount('#app')
+// Create Vue application
+const app = createApp(App)
+
+// Use router
+app.use(router)
+
+// Mount Vue application
+app.mount('#app')
